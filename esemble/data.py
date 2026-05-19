5aa3f35b90778110d2e26f45de4970ff7a40d0a6
LHLobato <luislobato35@gmail.com>
2026-05-19 15:19:19 -0300

    fix

--- esemble/data.py
+++ esemble/data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 
-def load_dataset_from_disk(image_path: str) -> list:
+def load_dataset_from_disk(image_path: str, domain_df:pd.DataFrame) -> list:
 
     base = Path(image_path)
 
@@ -11,7 +11,9 @@ def load_dataset_from_disk(image_path: str) -> list:
 
     all_files = malicious_files + benign_files
     all_domains = [s.split('-', 1)[-1] for s in all_files]
-
-    domain_df = pd.read_csv("../data/acme/val.csv")
     valid_domain = domain_df[domain_df['name'].isin(all_domains)]
+    print(f"[IMAGES] {len(all_files)}")
+    print(f"[DOMAINS] {len(domain_df)}")
+    print(f"[SUCCESS-RATE] {(len(valid_domain)/len(domain_df)):.2f}%!!!")
+
     return valid_domain
--- esemble/test.py
+++ esemble/test.py
@@ -1,10 +1,21 @@
-from multimodal import DomainEnsemble
+from multimodal import Domain_Ensemble
 from data import load_dataset_from_disk
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("--image_path", type=str, help="images dataset")
+parser.add_argument("--llm_path", type=str, help="pre-trained llm path")
+parser.add_argument("--cnn_path", type=str, help="pre-trained cnn path")
+parser.add_argument("--tab_path", type=str, help="pre-trained tabular model path")
+
+args = parser.parse_args()
 
 
+#IMAGE_PATH = "/home/lhslobato/novo-reshaping/images/GADF/val"
+domain_df = pd.read_csv("../data/acme/val.csv")
 
-IMAGE_PATH = "/home/lhslobato/novo-reshaping/images/GADF/val"
-LLM_PATH = ""
-MLP_PATH = ""
+IMG_PATH = args.img_path
+LLM_PATH = args.llm_path
+MLP_PATH = args.tab_path
 
 print(len(load_dataset_from_disk(IMAGE_PATH)))
