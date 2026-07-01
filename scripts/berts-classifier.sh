81c3ee54c1b5ddb24e6d41ef7a794de3028db25f
LHLobato <luislobato35@gmail.com>
2026-07-01 13:36:14 -0300

    damn

--- scripts/berts-classifier.sh
+++ scripts/berts-classifier.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 #PATHS=("meta-llama/Llama-3.2-1B-Instruct" "google/gemma-3-1b-it" "Qwen/Qwen2.5-1.5B-Instruct")
 #MODELS=("Llama-3" "Gemma-2B" "Qwen")
-PATHS=("google-bert/bert-base-uncased")
-MODELS=("BERT-Base")
+#PATHS=("distilbert/distilbert-base-uncased" "google-bert/bert-base-uncased" "answerdotai/ModernBERT-Base" "microsoft/deberta-v3-base")
+#MODELS=("distilBERT" "BERT-Base" "ModernBERT" "DEBERta" )
+
+PATHS=("answerdotai/ModernBERT-Base" "microsoft/deberta-v3-base")
+MODELS=("ModernBERT" "DEBERTa" )
+
+DATASETS=("csic" "fwaf" "httpparams")
 echo "----------------------------------"
 echo "        INICIANDO TESTES"
 echo "----------------------------------"
 
-for i in "${!MODELS[@]}"; do
-    path=${PATHS[$i]}
-    model=${MODELS[$i]}
-    echo "Rodando: $model (Caminho: $path)"
+for DATA in "${!DATASETS[@]}"; do
+        for i in "${!MODELS[@]}"; do
+        path=${PATHS[$i]}
+        model=${MODELS[$i]}
+        dataset=${DATASETS[$DATA]}
+        echo "Rodando: $model (Caminho: $path)"
 
-    python ../training/bert_training.py \
-        --model "$model" \
-        --modelpath "$path" \
-        --dataset domain-enriched \
-<<<<<<< HEAD
-        --batch 64 \
-        --strategy "only-names" \
-        --num_epochs 5 \
-        --train_mode tuning
-=======
-        --strategy "name" \
-        --num_epochs 3 \
-        --dora --train_mode tuning
->>>>>>> d4b83e822435d191a649079d72d80de8e083888b
+        python ../training/bert_training.py \
+                --model "$model" \
+                --modelpath "$path" \
+                --dataset "$dataset" \
+                --batch 16 \
+                --num_epochs 10 \
+                --train_mode tuning
 
-    echo "Finalizado: $model"
-    echo "----------------------------------"
-done
-
+        echo "Finalizado: $model"
+        echo "----------------------------------"
+        done
+done 
 echo "Todos os testes foram concluídos com sucesso!"
